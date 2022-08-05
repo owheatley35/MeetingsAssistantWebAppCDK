8c61d5b89970dd088e21aeaf544fbe34cb6ce802
Oliver Wheatley <44225127+owheatley35@users.noreply.github.com>
2022-08-05 06:20:25 +0100

    Updating Config Values to New github ARN

--- lib/ConfigValues.ts
+++ lib/ConfigValues.ts
@@ -1,6 +1,6 @@
 export default class ConfigValues {
     // Github Config:
-    static GITHUB_CONNECTION_ARN: string = "arn:aws:codestar-connections:eu-west-2:276910988699:connection/82c97fb8-d8de-461e-989f-5bfe095f23de";
+    static GITHUB_CONNECTION_ARN: string = "arn:aws:codestar-connections:eu-west-2:276910988699:connection/10e347a2-154b-4a3a-9580-be135ae9a8e9";
     static GITHUB_WEBAPP_REPO: string = "owheatley35/meetings-assistant-static-website";
     static GITHUB_CDK_REPO: string = "owheatley35/MeetingsAssistantWebAppCDK";
     
