5d01be3fd2be285d4d175adb863f574573307db8
Oli Wheatley <owheatley35@gmail.com>
2022-08-25 11:05:44 +0100

    Using complete bucket

--- lib/app/api/constructs/meetings_assistant_api_lambda_construct.ts
+++ lib/app/api/constructs/meetings_assistant_api_lambda_construct.ts
@@ -19,14 +19,14 @@ export class MeetingsAssistantApiLambdaConstruct extends Construct {
             bucketName: `${props.stage.toLowerCase()}-api-webapp-lambda-deployment-bucket`
         });
         
-        new BucketDeployment(this, `${props.stage.toLowerCase()}-meetings-assistant-lambda-deploy`, {
+        const bucketDeployment = new BucketDeployment(this, `${props.stage.toLowerCase()}-meetings-assistant-lambda-deploy`, {
             sources: [Source.asset(path.join(__dirname, './res/lambda_function.zip'))],
             destinationBucket: this.lambdaDeploymentBucket,
             prune: false
         })
 
         this.lambdaFunction = new Function(this, `${props.stage.toLowerCase()}-api-lambda-function`, {
-            code: Code.fromBucket(this.lambdaDeploymentBucket, ConfigValues.LAMBDA_ZIP_NAME_WITH_EXT),
+            code: Code.fromBucket(bucketDeployment.deployedBucket, ConfigValues.LAMBDA_ZIP_NAME_WITH_EXT),
             description: "API Lambda Function for Meetings Assistant",
             handler: "lambda_handler.handle",
             runtime: Runtime.PYTHON_3_8,
