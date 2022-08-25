2d384709b09c6087903c5da77223a9c3d5d625b0
Oli Wheatley <owheatley35@gmail.com>
2022-08-25 10:00:11 +0100

    Adding deployment to S3 to ensure lambda creates

--- lib/app/api/constructs/meetings_assistant_api_lambda_construct.ts
+++ lib/app/api/constructs/meetings_assistant_api_lambda_construct.ts
@@ -3,6 +3,7 @@ import {Code, Function, Runtime} from "aws-cdk-lib/aws-lambda"
 import {Bucket, BucketAccessControl} from "aws-cdk-lib/aws-s3";
 import ConfigValues from "../../../ConfigValues";
 import {DefaultConstructProps} from "../../meetings_assistant_web_app_cdk-stack";
+import {BucketDeployment, Source} from "aws-cdk-lib/aws-s3-deployment";
 
 export class MeetingsAssistantApiLambdaConstruct extends Construct {
     public readonly lambdaFunction: Function;
@@ -16,6 +17,12 @@ export class MeetingsAssistantApiLambdaConstruct extends Construct {
             accessControl: BucketAccessControl.PRIVATE,
             bucketName: `${props.stage.toLowerCase()}-api-webapp-lambda-deployment-bucket`
         });
+        
+        new BucketDeployment(this, `${props.stage.toLowerCase()}-meetings-assistant-lambda-deploy`, {
+            sources: [Source.asset('./res/lambda_handler.zip')],
+            destinationBucket: this.lambdaDeploymentBucket,
+            prune: false
+        })
 
         this.lambdaFunction = new Function(this, `${props.stage.toLowerCase()}-api-lambda-function`, {
             code: Code.fromBucket(this.lambdaDeploymentBucket, ConfigValues.LAMBDA_ZIP_NAME_WITH_EXT),
