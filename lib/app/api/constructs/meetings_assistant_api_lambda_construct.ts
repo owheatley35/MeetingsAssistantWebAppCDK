df7b6b191a610a80863de9d5186bc1afb5bc1c0c
Oli Wheatley <owheatley35@gmail.com>
2022-08-28 21:06:44 +0100

    Updating bucket to draw code from

--- lib/app/api/constructs/meetings_assistant_api_lambda_construct.ts
+++ lib/app/api/constructs/meetings_assistant_api_lambda_construct.ts
@@ -3,8 +3,8 @@ import {Code, Function, Runtime} from "aws-cdk-lib/aws-lambda";
 import {Bucket, BucketAccessControl} from "aws-cdk-lib/aws-s3";
 import ConfigValues from "../../../ConfigValues";
 import {DefaultConstructProps} from "../../meetings_assistant_web_app_cdk-stack";
-import {BucketDeployment, Source} from "aws-cdk-lib/aws-s3-deployment";
-import * as path from "path";
+// import {BucketDeployment, Source} from "aws-cdk-lib/aws-s3-deployment";
+// import * as path from "path";
 
 export class MeetingsAssistantApiLambdaConstruct extends Construct {
     public readonly lambdaFunction: Function;
@@ -19,14 +19,14 @@ export class MeetingsAssistantApiLambdaConstruct extends Construct {
             bucketName: `${props.stage.toLowerCase()}-api-webapp-lambda-deployment-bucket`
         });
         
-        const bucketDeployment = new BucketDeployment(this, `${props.stage.toLowerCase()}-meetings-assistant-lambda-deploy`, {
-            sources: [Source.asset(path.join(__dirname, 'res/lambda_function'))],
-            destinationBucket: this.lambdaDeploymentBucket,
-            prune: false
-        })
+        // const bucketDeployment = new BucketDeployment(this, `${props.stage.toLowerCase()}-meetings-assistant-lambda-deploy`, {
+        //     sources: [Source.asset(path.join(__dirname, 'res/lambda_function'))],
+        //     destinationBucket: this.lambdaDeploymentBucket,
+        //     prune: false
+        // })
 
         this.lambdaFunction = new Function(this, `${props.stage.toLowerCase()}-api-lambda-function`, {
-            code: Code.fromBucket(bucketDeployment.deployedBucket, ConfigValues.LAMBDA_ZIP_NAME_WITH_EXT),
+            code: Code.fromBucket(this.lambdaDeploymentBucket, ConfigValues.LAMBDA_ZIP_NAME_WITH_EXT),
             description: "API Lambda Function for Meetings Assistant",
             handler: "lambda_handler.handle",
             runtime: Runtime.PYTHON_3_8,
