52f1f9a04f134694189bc416048e4c8e0f7f7342
Oli Wheatley <owheatley35@gmail.com>
2022-08-31 10:56:14 +0100

    Adding s3 bucket policy

--- lib/app/pipeline/lambdaupdater/lambda_updater_construct.ts
+++ lib/app/pipeline/lambdaupdater/lambda_updater_construct.ts
@@ -4,10 +4,15 @@ import * as path from "path";
 import {Effect, Policy, PolicyStatement} from "aws-cdk-lib/aws-iam";
 import {Stack} from "aws-cdk-lib";
 
+interface LambdaUpdaterConstructProps {
+    readonly lambdaARNsToBeUpdated: string[];
+    readonly s3ARNsToBeUpdated: string[];
+}
+
 class LambdaUpdaterConstruct extends Stack {
     public readonly lambdaFunction: Function;
     
-     constructor(scope: Construct, id: string, lambdaARNsToBeUpdated: string[]) {
+     constructor(scope: Construct, id: string, props: LambdaUpdaterConstructProps) {
          super(scope, id);
     
          this.lambdaFunction = new Function(this, `api-lambda-updater-function`, {
@@ -20,12 +25,21 @@ class LambdaUpdaterConstruct extends Stack {
          const lambdaPolicyStatement: PolicyStatement = new PolicyStatement({
              effect: Effect.ALLOW,
              actions: ['lambda:UpdateFunctionCode'],
-             resources: lambdaARNsToBeUpdated
+             resources: props.lambdaARNsToBeUpdated
          });
+         
+         const s3BucketPolicyStatement: PolicyStatement = new PolicyStatement({
+             effect: Effect.ALLOW,
+             actions: ['s3:GetObject'],
+             resources: props.s3ARNsToBeUpdated
+         })
     
          this.lambdaFunction.role?.attachInlinePolicy(
              new Policy(this, 'update-lambda-code-policy', {
-                 statements: [lambdaPolicyStatement]
+                 statements: [
+                     lambdaPolicyStatement,
+                     s3BucketPolicyStatement
+                 ]
              }),
          );
      }
